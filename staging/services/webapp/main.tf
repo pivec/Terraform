766721616bb512488eea7f05af77dcf68b0ff8ec
Hunter Quinn <hpq1994@gmail.com>
2018-03-15 11:18:43 -0400

    Fixed syntax error

--- staging/services/webapp/main.tf
+++ staging/services/webapp/main.tf
@@ -108,17 +108,17 @@ data "terraform_remote_state" "db" {
   backend = "s3"
 
   config {
-    bucket = "(hquinn-tf-s3-bucket)"
+    bucket = "hquinn-tf-s3-bucket"
     key    = "staging/data-stores/mysql/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 data "template_file" "user_data" {
-  template = "${file("user-data.sh")}""
+  template = "${file("user-data.sh")}"
 
   vars {
-    server_port = "$"{var.server_port}"
+    server_port = "${var.server_port}"
     db_address  = "${data.terraform_remote_state.db.address}"
     db_port     = "${data.terraform_remote_state.db.port}"
   }
